6dcf5f2662ceaa0d298f6423019a44dfcc46b6bb
Mathew Moore <mat.moore@users.noreply.github.com>
2022-08-15 11:28:49 +0100

    Add config and add commands
    https://hyperskill.org/projects/177/stages/910/implement

--- src/main/kotlin/CLI.kt
+++ src/main/kotlin/CLI.kt
@@ -1,5 +1,6 @@
 package svcs
 
+import java.io.File
 import kotlin.system.exitProcess
 
 val helpMessages = mapOf(
@@ -10,6 +11,8 @@ val helpMessages = mapOf(
   "checkout" to "Restore a file."
 )
 
+val commands = mapOf("config" to ::config, "add" to ::add)
+
 fun printHelp() {
   println("These are SVCS commands:")
   for((name, desc) in helpMessages.entries) {
@@ -17,15 +20,105 @@ fun printHelp() {
   }
 }
 
+fun printCommandHelp(name: String) {
+  println("%-11s %s".format(name, helpMessages[name]!!))
+}
+
+fun config(args: Array<String>) {
+  if(args.isEmpty()) {
+    val name = loadName()
+    if (name === null) {
+      println("Please, tell me who you are.")
+    } else {
+      println("The username is $name.")
+    }
+  } else {
+    val name = args[0]
+    storeConfig(mapOf("name" to name))
+    println("The username is $name.")
+  }
+}
+
+fun add(args: Array<String>) {
+  val index = loadIndex().toMutableSet()
+
+  if(args.isEmpty()) {
+    if (index.isEmpty()) {
+      println("Add a file to the index.")
+    } else {
+      println("Tracked files:")
+      index.forEach { println(it) }
+    }
+  } else {
+    args.forEach {
+      if(!File(it).exists()) {
+        println("Can't find '$it'.")
+        exitProcess(1)
+      }
+      index.add(it)
+      println("The file '$it' is tracked.")
+    }
+    saveIndex(index)
+  }
+}
+
+fun storeConfig(values: Map<String, String>) {
+  val encoded = values.map { "${it.key}: ${it.value}" }.joinToString("\n") + "\n"
+  val configFile = File("vcs/config.txt")
+  configFile.writeText(encoded)
+}
+
+fun loadConfig(): Map<String, String>? {
+  val configFile = File("vcs/config.txt")
+  if (!configFile.exists()) {
+    return null
+  }
+
+  return configFile.readText().split("\n").associate {
+    val entry = it.split(": ")
+    entry.first() to entry.last()
+  }
+}
+
+fun loadIndex(): Set<String> {
+  val indexFile = File("vcs/index.txt")
+  if (!indexFile.exists()) {
+    return setOf()
+  }
+
+  return indexFile.readText().split("\n").toSet()
+}
+
+fun saveIndex(index: Set<String>) {
+  val indexFile = File("vcs/index.txt")
+  val encoded = index.joinToString("\n")
+  indexFile.writeText(encoded)
+}
+
+fun loadName(): String? {
+  val config = loadConfig() ?: return null
+  return config["name"]
+}
+
+fun ensureVCSDirectoryExists() {
+  File("vcs").mkdirs()
+}
+
 fun main(args: Array<String>) {
-  if (args.size != 1) {
+  if (args.isEmpty()) {
     printHelp()
     exitProcess(0)
   }
 
+  ensureVCSDirectoryExists()
+
   when (val command = args[0]) {
     "--help" -> {
       printHelp()
+    }
+
+    in commands -> {
+      commands[command]?.invoke(args.drop(1).toTypedArray())
     }
 
     in helpMessages -> {
