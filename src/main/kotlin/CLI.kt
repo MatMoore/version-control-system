53187e185de53df8aa1def8977bc4d7ce8f837ee
Mathew Moore <mat.moore@users.noreply.github.com>
2022-09-08 13:44:51 +0100

    Implement  commits and log
    https://hyperskill.org/projects/177/stages/911/implement

--- src/main/kotlin/CLI.kt
+++ src/main/kotlin/CLI.kt
@@ -1,6 +1,8 @@
 package svcs
 
 import java.io.File
+import java.math.BigInteger
+import java.security.MessageDigest
 import kotlin.system.exitProcess
 
 val helpMessages = mapOf(
@@ -11,7 +13,14 @@ val helpMessages = mapOf(
   "checkout" to "Restore a file."
 )
 
-val commands = mapOf("config" to ::config, "add" to ::add)
+val commands = mapOf(
+  "config" to ::config,
+  "add" to ::add,
+  "commit" to ::commit,
+  "log" to ::log
+)
+
+data class LogEntry(val author: String, val commitHash: String, val message: String)
 
 fun printHelp() {
   println("These are SVCS commands:")
@@ -62,6 +71,111 @@ fun add(args: Array<String>) {
   }
 }
 
+
+/**
+ * Commit the contents of the index to the repository by
+ * copying files to a subfolder
+ * Note that this doesn't do any validation of the index
+ * and assumes that no paths begin with '../'
+ */
+fun commit(args: Array<String>) {
+  if(args.isEmpty()) {
+    println("Message was not passed.")
+    exitProcess(1)
+  }
+
+  val message = args.first()
+  val author = getAuthor()
+
+  if(author == null) {
+    println("Please configure your name first.")
+    printCommandHelp("config")
+    exitProcess(1)
+  }
+
+  val stagedFiles = loadIndex()
+  val commitHash = generateCommitHash(stagedFiles, message)
+
+  for (stagedFile in stagedFiles) {
+    val origin = File(stagedFile)
+    val destination = File("vcs/commits/${commitHash}/${stagedFile}")
+    destination.parentFile.mkdirs()
+    origin.copyTo(destination)
+  }
+
+  saveIndex(setOf())
+  appendToLog(commitHash, message, author)
+}
+
+fun log(args: Array<String>) {
+  val logEntries = loadLog().reversed()
+  if (logEntries.isEmpty()) {
+    println("No commits")
+    return
+  }
+
+  for (entry in logEntries.dropLast(1)) {
+    println("commit ${entry.commitHash}")
+    println("author ${entry.author}")
+    println(entry.message)
+    println()
+  }
+
+  val last = logEntries.last()
+  println("commit ${last.commitHash}")
+  println("author ${last.author}")
+  println(last.message)
+}
+
+/**
+ * Generate a commit hash based on the hash of the changed file contents,
+ * their filenames, and the commit message.
+ * FIXME: this will create collisions if we make the same commit twice - this
+ * case is not handled
+ */
+fun generateCommitHash(filenames: Set<String>, message: String): String {
+  val sha256 = MessageDigest.getInstance("SHA-256")
+  sha256.update(message.toByteArray())
+
+  for (filename in filenames.sorted()) {
+    val file = File(filename)
+    sha256.update(filename.toByteArray())
+    sha256.update(file.inputStream().readAllBytes())
+  }
+
+  return "%032x".format(BigInteger(1, sha256.digest()))
+}
+
+fun appendToLog(commitHash: String, message: String, author: String) {
+  val logFile = File("vcs/log.txt")
+  val formattedLog = "${commitHash} | ${author}: ${message}\n"
+  logFile.appendText(formattedLog)
+}
+
+fun loadLog() : List<LogEntry> {
+  val logFile = File("vcs/log.txt")
+
+  if (!logFile.exists()) {
+    return listOf()
+  }
+
+  val contents = logFile.readText().trimEnd()
+  if(contents == "") {
+    return listOf()
+  }
+
+  val logRegex = Regex("^(?<commitHash>[a-f\\d]+) \\| (?<author>[^:]+): (?<message>.*)")
+
+  return contents.split("\n").map {
+    val matchResult = logRegex.matchEntire(it) ?: throw Exception("Log file corrupted")
+    LogEntry(
+      matchResult.groups["author"]!!.value,
+      matchResult.groups["commitHash"]!!.value,
+      matchResult.groups["message"]!!.value
+    )
+  }
+}
+
 fun storeConfig(values: Map<String, String>) {
   val encoded = values.map { "${it.key}: ${it.value}" }.joinToString("\n") + "\n"
   val configFile = File("vcs/config.txt")
@@ -80,13 +194,17 @@ fun loadConfig(): Map<String, String>? {
   }
 }
 
+fun getAuthor() : String? = loadConfig()?.get("name")
+
 fun loadIndex(): Set<String> {
   val indexFile = File("vcs/index.txt")
   if (!indexFile.exists()) {
     return setOf()
   }
 
-  return indexFile.readText().split("\n").toSet()
+  val lines = indexFile.readText().split("\n").toSet()
+
+  return lines.minusElement("") // splitting an empty string returns a list of empty string >:(
 }
 
 fun saveIndex(index: Set<String>) {
@@ -101,7 +219,7 @@ fun loadName(): String? {
 }
 
 fun ensureVCSDirectoryExists() {
-  File("vcs").mkdirs()
+  File("vcs/commits").mkdirs()
 }
 
 fun main(args: Array<String>) {
